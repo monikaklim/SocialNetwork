1043a053b9f04a96d0b9ba9522c6abaa247b053d
monikaklim <monyklim@gmail.com>
2020-01-07 11:39:02 +0100

    created method showLogin

--- src/it/monikaklim/socialnetwork/controller/SocialNetworkController.java
+++ src/it/monikaklim/socialnetwork/controller/SocialNetworkController.java
@@ -1,5 +1,32 @@
 package it.monikaklim.socialnetwork.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SocialNetworkController {
 
+	
+	@RequestMapping("/")
+	public String showLogin() {
+
+	return "login";
+	}		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
